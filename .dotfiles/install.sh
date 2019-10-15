2594d62f699a12ce2813b59a9e8157ab9972253e
MervanA <mervan.abughoush@gmail.com>
2019-10-15 22:19:35 +0300

    very simple installer

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
  
 declare -a dotfiles=(
-.bashrc
-.bash_profile
-.inputrc
-.vimrc
-.dotfiles
+${HOME}/dotfiles/.bashrc
+${HOME}/dotfiles/.bash_profile
+${HOME}/dotfiles/.inputrc
+${HOME}/dotfiles/.vimrc
+${HOME}/dotfiles/.dotfiles
 )
 
 for i in ${dotfiles[@]} ; do
