5f5c237cb905c9d28772f75a680157ee3ce76460
MervanA <mervan.abughosh@outlook.com>
2023-03-15 00:16:54 +0300

    working

--- install.sh
+++ install.sh
@@ -8,14 +8,18 @@ destdir="${HOME}"
 
 # Copy all files and directories from the source directory to the destination directory,
 # excluding the .git directory
-rsync -av --exclude='.git' "${srcdir}/" "${destdir}/"
+echo "Installing dotfiles: "
+rsync -a --exclude='.git' "${srcdir}/" "${destdir}/" | while read -r _; do
+  echo -n "."
+  sleep 0.05
+done
 
 # Source the new .bashrc file
 # shellcheck source=/dev/null
 source "${HOME}/.bashrc"
 
-# Remove the source directory
-rm -rf "${srcdir}"
+# Remove the directory containing the install script
+rm -rf "$(dirname "${BASH_SOURCE[0]}")"
 
 # Output a message indicating that the installation is complete
 printf '%s\n\n' "Dotfiles installed to ${destdir}"
